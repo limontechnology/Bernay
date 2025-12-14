ded8095529208842d36e77ee2177289a850cd51c
David Limon <limon7echnology@gmail.com>
2025-12-14 11:02:01 -0600

    Functions were added to the dark-mode button

--- main.js
+++ main.js
@@ -8,3 +8,16 @@ function myFunction() {
     var scrolled = (winScroll / height) * 100;
     document.getElementById("myBar").style.width = scrolled + "%";
 }
+
+// theme-btn
+
+const themeToggle = document.getElementById('themeToggle');
+const body = document.body;
+
+themeToggle.addEventListener('change', () => {
+    if (themeToggle.checked) {
+        body.classList.add('dark-mode');
+    } else {
+        body.classList.remove('dark-mode');
+    }
+});
