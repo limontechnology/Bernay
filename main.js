2d13962e231dc777ffe69e34e8cce485406bf0f8
David Limon <limon7echnology@gmail.com>
2026-04-24 01:10:07 -0600

    A slide is added, and the logo and color palette are changed.

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
+// Show Slides
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName('mySlides');
+  let dots = document.getElementsByClassName('dot');
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].classList.remove('slide-active');
+  }
+
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].classList.add('slide-active');
+}
+
+setInterval(() => {
+  plusSlides(1);
+}, 8000);
+
 // Top ProgressBar 
 
 window.onscroll = function() {myFunction()};
@@ -28,8 +64,7 @@ var animatedAccordion = document.getElementsByClassName('accordion');
 
 for (var i = 0; i < animatedAccordion.length; i++) {
   animatedAccordion[i].addEventListener('click', function() {
-    this.classList.toggle('active'); // cambia el simbolo
-
+    this.classList.toggle('accordion-active'); 
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
